a81be9ba3302c070199ec685fad559e09a228185
isaiahden <isaiahlayefa@gmail.com>
2025-07-25 19:33:58 +0100

    Update skillswapapp9.py

--- skillswapapp9.py
+++ skillswapapp9.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
 from datetime import datetime
 
 # ---------------- FIREBASE SETUP ----------------
