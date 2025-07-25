6d02a12e75ab1ca4388af1e13f5b1378c9db67bb
isaiahden <isaiahlayefa@gmail.com>
2025-07-25 13:09:19 +0100

    Update skillswapapp9.py

--- skillswapapp9.py
+++ skillswapapp9.py
@@ -5,7 +5,18 @@ from datetime import datetime
 
 # ---------------- FIREBASE SETUP ----------------
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
+    cred = credentials.Certificate({
+        "type": st.secrets.FIREBASE.type,
+        "project_id": st.secrets.FIREBASE.project_id,
+        "private_key_id": st.secrets.FIREBASE.private_key_id,
+        "private_key": st.secrets.FIREBASE.private_key.replace("\\n", "\n"),
+        "client_email": st.secrets.FIREBASE.client_email,
+        "client_id": st.secrets.FIREBASE.client_id,
+        "auth_uri": st.secrets.FIREBASE.auth_uri,
+        "token_uri": st.secrets.FIREBASE.token_uri,
+        "auth_provider_x509_cert_url": st.secrets.FIREBASE.auth_provider_x509_cert_url,
+        "client_x509_cert_url": st.secrets.FIREBASE.client_x509_cert_url
+    })
     firebase_admin.initialize_app(cred)
 db = firestore.client()
 
