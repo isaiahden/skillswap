e0270149cddeab9636d41a9ac463e767a722abd7
isaiahden <isaiahlayefa@gmail.com>
2025-07-28 09:29:07 +0100

    Update skillswapapp9.py

--- skillswapapp9.py
+++ skillswapapp9.py
@@ -7,7 +7,7 @@ import hashlib
 
 # ---------------- FIREBASE SETUP ----------------
 if not firebase_admin._apps:
-    cred = credentials.Certificate(dict(st.secrets["firebase"]))
+    cred = credentials.Certificate(dict(st.secrets["FIREBASE"]))
     firebase_admin.initialize_app(cred)
 db = firestore.client
 
